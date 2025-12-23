55a4b899503f7a3f3207918fffd24a51f7aea36b
Ahmed Eliewa <117938084+AhmedEliewa@users.noreply.github.com>
2025-12-23 19:21:46 +0200

    Enable navigation and project components in App
    Uncommented component imports and usage in App.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-// import Nav from "./components/Nav/Nav";
-// import Home from "./components/Home/Home";
-// import About from "./components/about/About";
-// import Skills from "./components/skills/Skills";
-// import Projects from "./components/Projects/Projects";
+import Nav from "./components/Nav/Nav";
+import Home from "./components/Home/Home";
+import About from "./components/about/About";
+import Skills from "./components/skills/Skills";
+import Projects from "./components/Projects/Projects";
 import Contact from "./components/contact/Contact";
 
 function App() {
   return (
     <div>
-      {/* <Nav />
+     <Nav />
       <Home />
       <About />
       <Skills />
-      <Projects /> */}
+      <Projects /> 
       <Contact />
     </div>
   );
